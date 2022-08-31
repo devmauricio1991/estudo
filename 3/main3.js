737f63d7b19fd9d35d771af76a6fe6acaf744639
Mauricio <ma-uh@hotmail.com>
2022-08-31 09:15:24 -0300

    Adicionando mais modulos ao estudo

---
+++ 3/main3.js
@@ -0,0 +1,15 @@
+function soma(x,y){
+    return x+y;
+}
+
+function subtracao(x,y){
+    return x-y;
+}
+
+export default {soma, subtracao};
+
+function multiplica(x,y){
+    return x*y
+}
+
+export default multiplica;
---
+++ 3/test3.js
@@ -0,0 +1,8 @@
+//Método para importar um módulo inteiro
+const operacoes = require("./main3.js").default;
+console.log(operacoes.soma(4,3));
+console.log(operacoes.subtrcao(4,3));
+
+//Médodo para imoprtar
+const multiplica = require("./main3.js").default;
+console.log(multiplica(4,3));
